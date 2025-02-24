4562be9f02ef8c24fda2a6f9a94f593e6b327698
MetteMunch <meha0005@stud.kea.dk>
2025-02-24 15:49:47 +0100

    js formregion er færdiggjort, hvor der gemmes en region der er hardkodet

--- js/formregion.js
+++ js/formregion.js
@@ -2,8 +2,6 @@ console.log("Jeg er i formregion")
 
 //Her vil jeg gemme en region oprettet via formularen fra html
 
-const feltInpName = document.getElementById("inpName")
-
 
 document.addEventListener('DOMContentLoaded', createFormEventListener);
 let formRegion;
