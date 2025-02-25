42b11d3d79640c00a3246f98aefccb8229005105
MetteMunch <meha0005@stud.kea.dk>
2025-02-25 13:16:19 +0100

    Har tilføjet deleteknap med uden action

--- js/kommunetable.js
+++ js/kommunetable.js
@@ -1,6 +1,6 @@
 import{fetchAnyUrl} from "./modulejson.js";
 
-console.log("Jeg er i kommunetable js)")
+console.log("Jeg er i kommunetable js")
 
 const urlKommuner = "http://localhost:8080/kommuner"
 const pbHentKommuner = document.getElementById("pbGetKommuner")
@@ -29,8 +29,36 @@ async function fetchKommuner() {
 
 function createTable(kommune) {
     let cellCount = 0
-    let rowCount = tblKommuner.rows.length
+    let rowCount = tblKommuner.rows.length //tblKommuner er HTML-tabellen
     console.log("rowCount", rowCount)
-    let row = tblKommuner.insertRow(rowCount)
+    let row = tblKommuner.insertRow(rowCount) //Her indsættes
+    //en ny række i tabellen på den næste ledige plads
     console.log("Kommune", kommune)
+
+    let cell = row.insertCell(cellCount++)//Her oprettes første
+    //celle i rækken
+    cell.innerHTML = kommune.kode //Her tildeles cellen en værdi
+
+    cell = row.insertCell(cellCount++)
+    cell.innerHTML = kommune.navn
+
+    cell = row.insertCell(cellCount++)
+    cell.innerHTML = kommune.href
+
+    cell = row.insertCell(cellCount++)
+    cell.innerHTML = kommune.region.kode
+
+    cell = row.insertCell(cellCount++)
+    cell.innerHTML = kommune.region.navn
+
+    cell = row.insertCell(cellCount++) //Her indsætter vi en deleteknap direkte
+    //i js
+    const pbDelete = document.createElement("input")
+    pbDelete.type = "button"
+    pbDelete.setAttribute("value","Slet kommune")
+    cell.appendChild(pbDelete) //Her knyttes knappen til cellen
+
+
+    console.log("row", row)
+
 }
